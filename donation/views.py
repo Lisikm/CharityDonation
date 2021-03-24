726487001eba01b27dba5601eb6b246683a06cd3
Lisikm <lisikm@gmail.com>
2021-03-24 14:29:16 +0100

    config and templates

--- donation/views.py
+++ donation/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, "base.html")
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, "adddonation.html")
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "register.html")
